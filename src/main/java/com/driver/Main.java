263ccac0a018098dd542228c72cb14292088b68b
Likhith Sai Charan Jupudi <jupudilikhithsaicharan@gmail.com>
2025-01-18 16:30:01 +0530

    completed the assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,13 +2,15 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        // F1 Car Instance
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        mercedes.accelerate(-60); // Now correctly reduces speed instead of setting it to -60
 
-        Boat tata = new Boat("tata",500);
-        System.out.println(tata.getVehicleName());
-        System.out.println(tata.getVehicleCapacity());
+        // Boat Instance
+        Boat myBoat = new Boat("Sea Explorer", 10);
+        System.out.println("Boat Name: " + myBoat.getVehicleName());
+        System.out.println("Boat Capacity: " + myBoat.getVehicleCapacity());
     }
 }
